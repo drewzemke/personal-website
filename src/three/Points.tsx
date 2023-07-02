0c6a2637eda03da948e01d07c4beb94b19bbff52
Drew Zemke <drew.zemke@gmail.com>
2023-07-02 16:16:09 -0700

    refactor: cleaned up `Points.tsx` a bit

--- src/three/Points.tsx
+++ src/three/Points.tsx
@@ -11,7 +11,6 @@ type PointData = {
 
 const Y_MIN = -5;
 const Y_MAX = 5;
-
 const RADIUS = 10;
 
 const NUM_R = 10;
@@ -20,7 +19,24 @@ const NUM_Y = 11;
 const ANG_VEL_MAX = 0.1;
 const ANG_VEL_MIN = 0.05;
 
-const getPointCoords = (point: PointData, t: number) => {
+const initialPoints = (): PointData[] =>
+  Array(NUM_Y)
+    .fill(0)
+    .flatMap((_, i) =>
+      Array(NUM_R)
+        .fill(0)
+        .map((_, j) => {
+          const theta = 2 * Math.PI * Math.random();
+          const radius = ((j + 1) / NUM_R) * RADIUS;
+          const y = (i / NUM_Y) * (Y_MAX - Y_MIN) + Y_MIN;
+          const ang_vel =
+            Math.sign(2 * Math.random() - 1) *
+            ((ANG_VEL_MAX - ANG_VEL_MIN) * Math.random() + ANG_VEL_MIN);
+          return { y, theta, radius, ang_vel };
+        })
+    );
+
+const computePointCoords = (point: PointData, t: number) => {
   const x = point.radius * Math.cos(point.theta + point.ang_vel * t);
   const z = point.radius * Math.sin(point.theta + point.ang_vel * t);
   return { x, y: point.y, z };
@@ -28,36 +44,18 @@ const getPointCoords = (point: PointData, t: number) => {
 
 function Point(point: PointData) {
   const ref = useRef<THREE.InstancedMesh>();
+
   useFrame(({ clock }) => {
     const elapsed = clock.getElapsedTime();
-    const { x, y, z } = getPointCoords(point, elapsed);
-    ref.current?.position.setX(x);
-    ref.current?.position.setY(y);
-    ref.current?.position.setZ(z);
+    const { x, y, z } = computePointCoords(point, elapsed);
+    ref.current?.position.set(x, y, z);
   });
+
   return <Instance ref={ref} />;
 }
 
 export function Points() {
-  const points: PointData[] = useMemo(
-    () =>
-      Array(NUM_Y)
-        .fill(0)
-        .flatMap((_, i) =>
-          Array(NUM_R)
-            .fill(0)
-            .map((_, j) => {
-              const theta = 2 * Math.PI * Math.random();
-              const radius = ((j + 1) / NUM_R) * RADIUS;
-              const y = (i / NUM_Y) * (Y_MAX - Y_MIN) + Y_MIN;
-              const ang_vel =
-                Math.sign(2 * Math.random() - 1) *
-                ((ANG_VEL_MAX - ANG_VEL_MIN) * Math.random() + ANG_VEL_MIN);
-              return { y, theta, radius, ang_vel };
-            })
-        ),
-    []
-  );
+  const points: PointData[] = useMemo(initialPoints, []);
 
   return (
     <Instances limit={points.length}>
