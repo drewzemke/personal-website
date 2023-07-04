c261774b45051b78d4b68e7b20c0f86eaca118bc
Drew Zemke <drew.zemke@gmail.com>
2023-07-03 23:11:52 -0700

    feat(content): replaced placeholder message with actual texts and links and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import { ThreeBackground } from './three/ThreeBackground';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 export function App() {
   return (
     <div className="container">
       <div className="overlay">
-        <div className="content blur">
+        <main className="content blur">
           <h1>Hi, I'm Drew!</h1>
           <p>
-            I'm currently rebulding my website.
-            <br />
-            Check back soon!
+            I do web development professionally but my true interests lie in mathematics and
+            computer graphics.
           </p>
-        </div>
+          <p>Check out the links below to learn more about me.</p>
+          <div className="social-links">
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/drewzemke/">
+                  <FaLinkedin className="icon" />
+                  <span>LinkedIn</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/drewzemke">
+                  <FaGithubSquare className="icon" />
+                  <span>GitHub</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </main>
       </div>
       <ThreeBackground />
     </div>
