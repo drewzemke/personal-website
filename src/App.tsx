de9e7db1099d745cd71703fb513914a3758ec8c6
Drew Zemke <drew.zemke@gmail.com>
2023-07-30 16:32:58 -0700

    refactor(app): organize into `pages` and `components` folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,5 @@
-import { ThreeBackground } from './three/ThreeBackground';
-import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { HomePage } from './pages/HomePage';
 
 export function App() {
-  return (
-    <div className="container">
-      <div className="overlay">
-        <main className="content blur">
-          <h1>Hi, I'm Drew!</h1>
-          <p>
-            I do web development professionally but my true interests lie in mathematics and
-            computer graphics.
-          </p>
-          <p>Check out the links below to learn more about me.</p>
-          <div className="social-links">
-            <ul>
-              <li>
-                <a href="https://www.linkedin.com/in/drewzemke/">
-                  <FaLinkedin className="icon" />
-                  <span>LinkedIn</span>
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/drewzemke">
-                  <FaGithubSquare className="icon" />
-                  <span>GitHub</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </main>
-      </div>
-      <ThreeBackground />
-    </div>
-  );
+  return <HomePage />;
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,36 @@
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { ThreeBackground } from '../components/three-background/ThreeBackground';
+
+export function HomePage() {
+  return (
+    <div className="container">
+      <div className="overlay">
+        <main className="content blur">
+          <h1>Hi, I'm Drew!</h1>
+          <p>
+            I do web development professionally but my true interests lie in mathematics and
+            computer graphics.
+          </p>
+          <p>Check out the links below to learn more about me.</p>
+          <div className="social-links">
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/drewzemke/">
+                  <FaLinkedin className="icon" />
+                  <span>LinkedIn</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/drewzemke">
+                  <FaGithubSquare className="icon" />
+                  <span>GitHub</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
+      <ThreeBackground />
+    </div>
+  );
+}
